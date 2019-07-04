75fe9f70875f67ddd4cdb3e05ae5083176d1cb8f
Gabriel Martins <gsmartins96@hotmail.com>
2019-07-04 17:38:00 -0300

    tudo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   before_action :authorize, except: [:about]
   def index
+    @recipes = Recipe.new.all_recipes
+    @categories = Category.new.all_categories
+    @objeto = Category.new.category_view
   end
 
   def about
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,23 @@
 class Category < ApplicationRecord
 
+  def category_view
+    connection = ActiveRecord::Base.connection
+    connection.execute <<-SQL
+    CREATE VIEW "category_view" AS SELECT "categories".name as category_name, "dishes".name as dish_name, "recipes".name as recipe_name, "recipes".instructions, "recipes".photo_recipe FROM "categories" LEFT OUTER JOIN "dishes" ON "categories".id = "dishes".category_id LEFT OUTER JOIN "recipes" ON "dishes".id = "recipes".dish_id;
+    SQL
+
+    result = connection.execute <<-SQL
+    SELECT * FROM "category_view";
+    SQL
+    
+    connection.execute <<-SQL
+    DROP VIEW "category_view";
+    SQL
+    
+    result = JSON.parse(result.to_json)
+    return result
+  end
+  
   def all_categories
     connection = ActiveRecord::Base.connection
     result = connection.execute <<-SQL 
